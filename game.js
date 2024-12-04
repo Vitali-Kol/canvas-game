ff3f4072be6feab44a6234111bf98c53763f48f3
Vitali.Kolesnikov <vitali.kolesnikov@ivkhk.ee>
2024-12-04 13:18:04 +0200

    Keep the ball on the platform while moving

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
+const KEYS = {
+    LEFT: 37,
+    RIGHT: 39
+};
+
 let game = {
     ctx: null,
     platform: null,
@@ -11,20 +16,20 @@ let game = {
         platform: null,
         block: null
     },
+
     init: function() {
         this.ctx = document.getElementById("mycanvas").getContext("2d");
         this.setEvents();
     },
+
     setEvents() {
         window.addEventListener("keydown", e => {
-            if (e.keyCode === 37) {
-                this.platform.dx = -this.platform.velocity;
-            } else if (e.keyCode === 39) {
-                this.platform.dx = this.platform.velocity;
+            if (e.keyCode === KEYS.LEFT || e.keyCode === KEYS.RIGHT) {
+                this.platform.start(e.keyCode);
             }
         });
         window.addEventListener("keyup", e => {
-            this.platform.dx = 0;
+            this.platform.stop();
         });
     },
 
@@ -44,6 +49,7 @@ let game = {
             this.sprites[key].addEventListener("load", onImageLoad);
         }
     },
+
     create() {
         for (let row = 0; row < this.rows; row++) {
             for (let col = 0; col < this.cols; col++) {
@@ -54,9 +60,11 @@ let game = {
             }
         }
     },
+
     update() {
         this.platform.move();
     },
+
     run() {
         window.requestAnimationFrame(() => {
             this.update();
@@ -64,17 +72,20 @@ let game = {
             this.run();
         });
     },
+
     render() {
         this.ctx.drawImage(this.sprites.background, 0, 0);
         this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
         this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
         this.renderBlocks();
-    },  
+    },
+
     renderBlocks() {
         for (let block of this.blocks) {
             this.ctx.drawImage(this.sprites.block, block.x, block.y);
         }
     },
+
     start: function() {
         this.init();
         this.preload(() => {
@@ -83,3 +94,37 @@ let game = {
         });
     }
 };
+
+game.ball = {
+    x: 320,
+    y: 280,
+    width: 20,
+    height: 20
+};
+
+game.platform = {
+    velocity: 6,
+    dx: 0,
+    x: 280,
+    y: 300,
+    start(direction) {
+        if (direction === KEYS.LEFT) {
+            this.dx = -this.velocity;
+        } else if (direction === KEYS.RIGHT) {
+            this.dx = this.velocity;
+        }
+    },
+    stop() {
+        this.dx = 0;
+    },
+    move() {
+        if (this.dx) {
+            this.x += this.dx;
+            game.ball.x += this.dx;
+        }
+    }
+};
+
+window.addEventListener("load", () => {
+    game.start();
+});
