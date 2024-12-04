ecabd52c8c1403c853a53e1439bf3ddfb20c1579
Vitali.Kolesnikov <vitali.kolesnikov@ivkhk.ee>
2024-12-04 12:56:25 +0200

    Arrange objects on the screen

--- game.js
+++ game.js
@@ -1,17 +1,21 @@
 let game = {
     ctx: null,
+    ball: null,
+    platform: null,
     sprites: {
         background: null,
         ball: null,
         platform: null
     },
+
     init: function() {
         this.ctx = document.getElementById("mycanvas").getContext("2d");
     },
+
     preload(callback) {
         let loaded = 0;
         let required = Object.keys(this.sprites).length;
-        
+
         for (let key in this.sprites) {
             this.sprites[key] = new Image();
             this.sprites[key].src = "img/" + key + ".png";
@@ -23,16 +27,19 @@ let game = {
             });
         }
     },
+
     run() {
         window.requestAnimationFrame(() => {
             this.render();
         });
     },
+
     render() {
         this.ctx.drawImage(this.sprites.background, 0, 0);
-        this.ctx.drawImage(this.sprites.ball, 0, 0);
-        this.ctx.drawImage(this.sprites.platform, 0, 0);
+        this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+        this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
     },
+
     start: function() {
         this.init();
         this.preload(() => {
@@ -40,3 +47,19 @@ let game = {
         });
     }
 };
+
+game.ball = {
+    x: 320,
+    y: 280,
+    width: 20,
+    height: 20
+};
+
+game.platform = {
+    x: 280,
+    y: 300
+};
+
+window.addEventListener("load", () => {
+    game.start();
+});
